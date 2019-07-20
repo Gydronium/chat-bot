ea295c4e660fa5f002253c6081f4190a337e76a7
Gydronium <quickjuly@yandex.ru>
2019-07-20 18:49:48 +0300

    Для сервера

--- db/connection.py
+++ db/connection.py
@@ -4,7 +4,7 @@ db = PostgresqlDatabase(
     'mydb',  # Required by Peewee.
     user='postgres',  # Will be passed directly to psycopg2.
     password='1',  # Ditto.
-    host='127.0.0.1')  # Ditto.
+    host='postgres://kxqgaprjxzbtqm:d77ed83f11ba731a5ca99019bf21780dde5e187f745e3e4c87bf22dab17e797f@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d65m3tup9um7l4')  # Ditto.
 
 
 class BaseModel(Model):
