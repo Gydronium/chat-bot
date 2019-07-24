3291f913d1971855aa37b7ec7ffd7ce3205d6235
Gydronium <quickjuly@yandex.ru>
2019-07-24 16:41:56 +0300

    Date update

--- plugins/date.py
+++ plugins/date.py
@@ -3,7 +3,7 @@ import datetime
 
 plugin = Plugin(name="Date", description="Reply with send message")
 
-@plugin.on_startswith_text("plugin")
+@plugin.on_startswith_text("date")
 async def _(message, env):
     now = datetime.datetime.now()
     await env.reply(now.strftime("%d-%m-%Y %H:%M"))
