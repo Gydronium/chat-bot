c53e7ff58779368f240bfa510611d0f575f0a6a8
Gydronium <quickjuly@yandex.ru>
2019-08-19 17:53:09 +0300

    event_manager update

--- db/events.py
+++ db/events.py
@@ -7,14 +7,14 @@ db = PostgresqlDatabase(
     host='ec2-174-129-41-127.compute-1.amazonaws.com')  # Ditto.
 
 
-class BaseModel(Model):
+class BaseModel1(Model):
     """A base model that will use our Postgresql database"""
 
     class Meta:
         database = db
 
 
-class User(BaseModel):
+class User1(BaseModel1):
     id = IntegerField()
     date = DateTimeField()
     message = TextField()
@@ -25,8 +25,8 @@ class User(BaseModel):
 
 
 def insert_event(event_id, event_date, event_message, event_is_upcoming):
-    User.insert(id=event_id, date=event_date, message=event_message, is_upcoming=event_is_upcoming).execute()
+    User1.insert(id=event_id, date=event_date, message=event_message, is_upcoming=event_is_upcoming).execute()
 
 
 def get_all_events():
-    return User.select()
+    return User1.select()
