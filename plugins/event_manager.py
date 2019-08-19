3ba0519376ee1ceda8bf4c4b943e92a6630079b0
Gydronium <quickjuly@yandex.ru>
2019-08-19 18:01:10 +0300

    event_manager update

--- plugins/event_manager.py
+++ plugins/event_manager.py
@@ -8,12 +8,15 @@ plugin1 = Plugin(name="Register_event")
 
 @plugin1.on_text("register_event")
 async def _(message, env):
-    data = env.body.split(',')
-    message = data[0]
-    date = datetime.strptime(data[1], "%d%m%Y").date()
-    ev_id = int(round(datetime.now().timestamp() * 1000))
-    insert_event(event_id=ev_id, event_date=date, event_message=message, event_is_upcoming=False)
-    await env.reply("{}".format("Reg_event is successful"))
+    try:
+        data = env.body.split(',')
+        message = data[0]
+        date = datetime.strptime(data[1], "%d%m%Y").date()
+        ev_id = int(round(datetime.now().timestamp() * 1000))
+        insert_event(event_id=ev_id, event_date=date, event_message=message, event_is_upcoming=False)
+        await env.reply("{}".format("Reg_event is successful"))
+    except:
+        await env.reply("{}".format("There was an error"))
 
 
 plugin2 = Plugin(name="Get_upcoming_events")
